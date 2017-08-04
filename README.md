fa16258d5d4d61f548c1a521da35b34b62f635e1
Kazuhiro <knman7@hotmail.com>
2017-08-04 22:30:21 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Build
+g++ -o hastar.exe main.cpp hybrid_breadth_first.cpp hybrid_breadth_first.h -std=c+11
---
+++ hybrid_breadth_first.h
@@ -0,0 +1,86 @@
+#ifndef HYBRID_BREADTH_FIRST_H_
+#define HYBRID_BREADTH_FIRST_H_
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+
+#define M_PI 3.14159265359
+
+using namespace std;
+
+class HBF {
+public:
+
+    int NUM_THETA_CELLS = 90;
+    double SPEED = 1.45;
+    double LENGTH = 0.5;
+
+    vector<vector<int>>HEURISTIC = {
+    {30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,},
+    {29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,},
+    {28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,},
+    {27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,},
+    {26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,},
+    {25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,},
+    {24,23,22,21,20,19,18,17,16,15,14,13,12,11,10, 9,},
+    {23,22,21,20,19,18,17,16,15,14,13,12,11,10, 9, 8,},
+    {22,21,20,19,18,17,16,15,14,13,12,11,10, 9, 8, 7,},
+    {21,20,19,18,17,16,15,14,13,12,11,10, 9, 8, 7, 6,},
+    {20,19,18,17,16,15,14,13,12,11,10, 9, 8, 7, 6, 5,},
+    {19,18,17,16,15,14,13,12,11,10, 9, 8, 7, 6, 5, 4,},
+    {18,17,16,15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3,},
+    {17,16,15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2,},
+    {16,15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1,},
+    {15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,},
+    };
+
+    struct maze_s {
+        int f;
+        int g;  // iteration
+        double x;
+        double y;
+        double theta;
+        
+        bool operator<(const maze_s& a) const
+        {
+            return f < a.f;
+        }
+    };
+
+    struct maze_path {
+    
+        vector< vector< vector<maze_s> > > closed;
+        vector< vector< vector<maze_s> > > came_from;
+        maze_s final;
+
+    };
+
+
+    /**
+    * Constructor
+    */
+    HBF();
+
+    /**
+    * Destructor
+    */
+    virtual ~HBF();
+
+    
+    int theta_to_stack_number(double theta);
+
+    int idx(double float_num);
+
+    vector<maze_s> expand(maze_s state);
+
+    maze_path search(vector< vector<int> > grid, vector<double> start, vector<int> goal);
+
+    vector<maze_s> reconstruct_path(vector< vector< vector<maze_s> > > came_from, vector<double> start, HBF::maze_s final);
+    
+
+};
+
+#endif
